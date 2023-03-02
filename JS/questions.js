c01f1922346350f675ed70e2873b6eceb6a7c1ce
Jonathan Dawkins <21jdawkins@gmail.com>
2023-03-02 10:26:17 -0500

    finished code

---
+++ JS/questions.js
@@ -0,0 +1,85 @@
+var questions = [
+    {
+      title: "If Code were anatomy what would JavaScript be?",
+      choices: ["bones", "all answers", "muscles", "skin"],
+      answer: "muscles"
+    },
+    {
+      title: "Which language have we not used",
+      choices: [
+        "Java Scripy",
+        "CSS",
+        "HTML",
+        "Python"
+      ],
+      answer: "Python"
+    },
+    {
+      title: "Commonly used data types are:",
+      choices: ["strings", "booleans", "all answers", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "A div tag is used to?",
+      choices: [
+        "divide",
+        "add",
+        "subtract",
+        "multiply"
+      ],
+      answer: "divide"
+    },
+    {
+      title:
+        "Which answer is not a JavaScript error?",
+      choices: [
+        "Animation time errors",
+        "Load time errors",
+        "Run time errors",
+        "Logical Errors"
+      ],
+      answer: "Animation time errors"
+    },
+    {
+      title: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data type",
+        "None of the above",
+        "All of the above"
+      ],
+      answer: "Object data types"
+    },
+    {
+      title: "An if else statements conditions are inside of what?",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays can be used to:",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "Variables are used to?",
+      choices: ["store data", "remove objects", "call a function", "divid sections"],
+      answer: "store data"
+    },
+    {
+      title:
+        "A good way to check if the code your working on is running properly is?",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    },
+    {
+      title: "What can you use to get a message popup?",
+      choices: ["Alert", "Confirm", "Prompt", "All the above"],
+      answer: "All the above"
+    }
+  ];
